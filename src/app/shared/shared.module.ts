0e912ee0c7a5b68558c3fa5bff46a8cbb63d742b
jonathan_fambro <startek591@gmail.com>
2024-02-09 11:01:04 -0500

    Created the shared module and added the card module from primeng into application

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CardModule } from 'primeng/card';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CardModule
+    ],
+    exports: [
+        CardModule
+    ]
+})
+export class SharedModule {}
